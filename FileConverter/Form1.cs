7b5d5fed9bf77d199a7dafb237d369b3237576e0
Krzysztof Wróblewski <k.wroblewski@microsoft.wsei.edu.pl>
2019-10-08 18:32:12 +0200

    Add project files.

---
+++ FileConverter/Form1.cs
@@ -0,0 +1,45 @@
+﻿using FileConverter.FileConverterService;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            var result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                label1.Text = openFileDialog1.FileName;
+            }
+            else
+                return;
+            
+            IFileConverterService service = new FileConverterServiceClient();
+            string content = File.ReadAllText(openFileDialog1.FileName);
+            FileModel model = new FileModel
+            {
+                Name = openFileDialog1.FileName,
+                Content = content
+            };
+            var resultFile = service.ConvertToCSV(model);
+            File.WriteAllText(resultFile.Name , resultFile.Content);
+            
+            
+        }
+    }
+}
